9ae7c76542df4239a955d4ac2969b3480c47b674
Uthman Eqbal <60008549+UthmanPortSwigger@users.noreply.github.com>
2021-08-27 15:42:51 +0100

    Update Add-Headers-Python.py
    Remove unnecessary comments.

--- Custom-Headers/Python/Add-Headers-Python.py
+++ Custom-Headers/Python/Add-Headers-Python.py
@@ -3,32 +3,31 @@ from burp import IHttpListener
 from burp import IHttpRequestResponse
 from burp import IRequestInfo
 
-# BurpExtender Class contaning all functions used to interact with Burp Suite Extender API
+# implement BurpExtender Class
 class BurpExtender(IBurpExtender, IHttpListener):
 
     # define registerExtenderCallbacks: From IBurpExtender Interface
     def registerExtenderCallbacks(self, callbacks):
 
-        # keep a reference to our callbacks object (Burp Extensibility Feature)
+        # keep a reference to our callbacks object
         self._callbacks = callbacks
-        # obtain an extension helpers object (Burp Extensibility Feature)
-        # http://portswigger.net/burp/extender/api/burp/IExtensionHelpers.html
+        # obtain an extension helpers object
         self._helpers = callbacks.getHelpers()
-        # set our extension name that will display in Extender Tab
-        self._callbacks.setExtensionName("Add a custom header")
-        # register ourselves as an HTTP listener
+        # set our extension name that will display in Extender > Output Tab
+        self._callbacks.setExtensionName("Add-Headers-Python")
+        # register an HTTP listener
         callbacks.registerHttpListener(self)
 
     # define processHttpMessage: From IHttpListener Interface
     def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
         # only work on requests:
         if messageIsRequest:
-            request = messageInfo.getRequest()                                          #get Request from IHttpRequestResponse instance
+            request = messageInfo.getRequest()                                          # get Request from IHttpRequestResponse instance
             requestStr = self._callbacks.getHelpers().bytesToString(request)
             requestParsed = self._helpers.analyzeRequest(request)
             body = requestStr[requestParsed.getBodyOffset():]
             headers = requestParsed.getHeaders()
-            headers.add('Header1: value1')                                              #change header name and value here 
+            headers.add('Header1: value1')                                              # change header name and value here 
             httpRequest = self._callbacks.getHelpers().buildHttpMessage(headers, body)
             messageInfo.setRequest(httpRequest)
             return
