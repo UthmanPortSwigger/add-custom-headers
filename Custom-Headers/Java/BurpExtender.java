5f67372bef7306a22e459e36005f92a0b226f396
Uthman Eqbal <60008549+UthmanPortSwigger@users.noreply.github.com>
2021-09-14 15:45:33 +0100

    Remove port from URL + additional comments

--- Custom-Headers/Java/BurpExtender.java
+++ Custom-Headers/Java/BurpExtender.java
@@ -6,13 +6,13 @@ import java.util.ArrayList;
 import java.net.URL;
 
 public class BurpExtender implements IBurpExtender, IHttpListener {
-    public static IBurpExtenderCallbacks callbacks;
-	  public IExtensionHelpers helpers;
-    PrintWriter stdout;
+    private static IBurpExtenderCallbacks callbacks;
+	  private IExtensionHelpers helpers;
+    private PrintWriter stdout;
 
-     
-    private String[] urlMatchList = {};                                                   // change urlMatchList to your in-scope URL/(s). If empty, headers are added to all requests.
-    private String[] headersToAdd = {"Header1: value1", "Header2: value2"};               // adjust key:value pairs in headersToAdd as appropriate. 
+    private String[] urlMatchList = {"https://portswigger-labs.net"};                                  /* change URL(/s) in urlMatchList to match your scope. If empty, headers in headersToAdd are added to all requests 
+                                                                                                                URL(/s) without protocol also works e.g. portswigger-labs.net */
+    private String[] headersToAdd = {"Header1: value1", "Header2: value2"};                            // adjust key:value pairs in headersToAdd as appropriate
 
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks){
@@ -22,33 +22,34 @@ public class BurpExtender implements IBurpExtender, IHttpListener {
       callbacks.setExtensionName("Add-Headers-Java");
       callbacks.registerHttpListener(this);
     }
-
   
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
         if(messageIsRequest) {
           IRequestInfo rqInfo = helpers.analyzeRequest(messageInfo);
           URL url = rqInfo.getUrl();
-          stdout.println(url.toString());
-          if (urlMatchList.length == 0) {                    // if the urlMatchList is empty, add headers to all requests 
+          String urlString = url.getProtocol()+"://"+url.getHost()+url.getPath(); 
+          if (urlMatchList.length == 0) {                                                               // if the urlMatchList is empty, add headers to all requests 
             addHeaders(messageInfo, rqInfo);
           }
-          for (int i=0;i<urlMatchList.length; i++) {         // iterate through urlMatchList array in a loop 
-            if (url.toString() == urlMatchList[i]) {         // check if the request URL is in the urlMatchList
-              addHeaders(messageInfo, rqInfo);
-              break;
+          else {
+            for (int i=0;i<urlMatchList.length; i++) {                                                  // iterate through the URLs in the urlMatchList array in a loop
+              if (urlString.contains(urlMatchList[i])) {                                                // check if the request URL is in the urlMatchList
+                addHeaders(messageInfo, rqInfo);                                                       
+                break;
+              }
             }
           }
         }
     }
 
     private void addHeaders(IHttpRequestResponse messageInfo, IRequestInfo rqInfo) {
-      List<String> headers = new ArrayList<String>(rqInfo.getHeaders());
+      List<String> headers = new ArrayList<String>(rqInfo.getHeaders());                                // TO-DO: add some logic to detect if a header in headersToAdd already exists in the request. If it does, skip and add next header
       for (int j=0; j<headersToAdd.length; j++) {
         headers.add(headersToAdd[j]);
-      }
+        }
       String request = new String(messageInfo.getRequest());
       String messageBody = request.substring(rqInfo.getBodyOffset());
       byte[] updateMessage = helpers.buildHttpMessage(headers, messageBody.getBytes());
       messageInfo.setRequest(updateMessage);
     }
-}    
+} 
