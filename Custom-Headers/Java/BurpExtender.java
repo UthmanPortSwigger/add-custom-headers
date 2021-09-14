1f51197a000f84c6fb6dd07d8e7f06f97c58d7ef
Uthman Eqbal <60008549+UthmanPortSwigger@users.noreply.github.com>
2021-09-14 10:36:44 +0100

    Optimisation by @HannahLaw-Portswigger
    - addHeaders function created 
    
    - Added scope by checking for URLs in urlMatchList. If this is empty, headers will be added to all requests by default 
    
    - headersToAdd array (instead of repetition of headers.add previously used)

--- Custom-Headers/Java/BurpExtender.java
+++ Custom-Headers/Java/BurpExtender.java
@@ -3,35 +3,52 @@ package burp;
 import java.io.PrintWriter;
 import java.util.List;
 import java.util.ArrayList;
+import java.net.URL;
 
 public class BurpExtender implements IBurpExtender, IHttpListener {
-    //
-    // implement IBurpExtender
-    //
-
-    private IExtensionHelpers helpers;
+    public static IBurpExtenderCallbacks callbacks;
+	  public IExtensionHelpers helpers;
     PrintWriter stdout;
 
-    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
-    {
+     
+    private String[] urlMatchList = {};                                                   // change urlMatchList to your in-scope URL/(s). If empty, headers are added to all requests.
+    private String[] headersToAdd = {"Header1: value1", "Header2: value2"};               // adjust key:value pairs in headersToAdd as appropriate. 
+
+    @Override
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks){
+      BurpExtender.callbacks = callbacks;
       stdout = new PrintWriter(callbacks.getStdout(), true);
       helpers = callbacks.getHelpers();
       callbacks.setExtensionName("Add-Headers-Java");
       callbacks.registerHttpListener(this);
     }
 
-    public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo)
-    {
+  
+    public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
         if(messageIsRequest) {
-            IRequestInfo rqInfo = helpers.analyzeRequest(messageInfo);
-            List<String> headers = new ArrayList<String>(rqInfo.getHeaders());
-            headers.add("Header1: value1");                                             /* change first header name and value here */ 
-            headers.add("Header2: value2");                                             /* change second header name and value here */ 
-            String request = new String(messageInfo.getRequest());
-            String messageBody = request.substring(rqInfo.getBodyOffset());
-            byte[] updateMessage = helpers.buildHttpMessage(headers, messageBody.getBytes());
-            messageInfo.setRequest(updateMessage);
-
+          IRequestInfo rqInfo = helpers.analyzeRequest(messageInfo);
+          URL url = rqInfo.getUrl();
+          stdout.println(url.toString());
+          if (urlMatchList.length == 0) {                    // if the urlMatchList is empty, add headers to all requests 
+            addHeaders(messageInfo, rqInfo);
+          }
+          for (int i=0;i<urlMatchList.length; i++) {         // iterate through urlMatchList array in a loop 
+            if (url.toString() == urlMatchList[i]) {         // check if the request URL is in the urlMatchList
+              addHeaders(messageInfo, rqInfo);
+              break;
+            }
+          }
         }
+    }
+
+    private void addHeaders(IHttpRequestResponse messageInfo, IRequestInfo rqInfo) {
+      List<String> headers = new ArrayList<String>(rqInfo.getHeaders());
+      for (int j=0; j<headersToAdd.length; j++) {
+        headers.add(headersToAdd[j]);
+      }
+      String request = new String(messageInfo.getRequest());
+      String messageBody = request.substring(rqInfo.getBodyOffset());
+      byte[] updateMessage = helpers.buildHttpMessage(headers, messageBody.getBytes());
+      messageInfo.setRequest(updateMessage);
     }
 }    
