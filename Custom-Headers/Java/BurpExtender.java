9514fff156428d4a88ba7e20a987f6907936de85
Hannah Law <hannah.law@PSIT100313.local>
2021-09-21 15:25:22 +0100

    Duplicate headers detection functionality added.

--- Custom-Headers/Java/BurpExtender.java
+++ Custom-Headers/Java/BurpExtender.java
@@ -10,46 +10,77 @@ public class BurpExtender implements IBurpExtender, IHttpListener {
 	  private IExtensionHelpers helpers;
     private PrintWriter stdout;
 
-    private String[] urlMatchList = {"https://portswigger-labs.net"};                                  /* change URL(/s) in urlMatchList to match your scope. If empty, headers in headersToAdd are added to all requests 
-                                                                                                                URL(/s) without protocol also works e.g. portswigger-labs.net */
-    private String[] headersToAdd = {"Header1: value1", "Header2: value2"};                            // adjust key:value pairs in headersToAdd as appropriate
+    // Change URL(/s) in urlMatchList to match your scope. If empty, headers in headersToAdd are added to all request URL(/s). Excluding protocol also works e.g. portswigger-labs.net
+    private String[] urlMatchList = {"https://portswigger-labs.net"};
+    // Adjust key:value pairs in headersToAdd as appropriate
+    private String[] headersToAdd = {"Accept-Encoding: gzip, deflateX"};
+    // Toggle checkForDuplicates between false and true if you want to remove any duplicated headers
+    private boolean checkForDuplicates = false;
 
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks){
+      // Initialize extension
       BurpExtender.callbacks = callbacks;
       stdout = new PrintWriter(callbacks.getStdout(), true);
       helpers = callbacks.getHelpers();
       callbacks.setExtensionName("Add-Headers-Java");
       callbacks.registerHttpListener(this);
     }
-  
+
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
-        if(messageIsRequest) {
-          IRequestInfo rqInfo = helpers.analyzeRequest(messageInfo);
-          URL url = rqInfo.getUrl();
-          String urlString = url.getProtocol()+"://"+url.getHost()+url.getPath(); 
-          if (urlMatchList.length == 0) {                                                               // if the urlMatchList is empty, add headers to all requests 
-            addHeaders(messageInfo, rqInfo);
-          }
-          else {
-            for (int i=0;i<urlMatchList.length; i++) {                                                  // iterate through the URLs in the urlMatchList array in a loop
-              if (urlString.contains(urlMatchList[i])) {                                                // check if the request URL is in the urlMatchList
-                addHeaders(messageInfo, rqInfo);                                                       
-                break;
-              }
+      // Only edit requests
+      if(messageIsRequest) {
+        // Get details of request
+        IRequestInfo rqInfo = helpers.analyzeRequest(messageInfo);
+        URL url = rqInfo.getUrl();
+        String urlString = url.getProtocol()+"://"+url.getHost()+url.getPath();
+        // if the urlMatchList is empty, add headers to all requests
+        if (urlMatchList.length == 0) {
+          addHeaders(messageInfo, rqInfo);
+        }
+        else {
+          // Iterate through URLs in urlMatchList
+          for (String match : urlMatchList) {
+            // If a URL contains a match to the urlMatchList, run the addHeaders function
+            if (urlString.contains(match)) {
+              addHeaders(messageInfo, rqInfo);
+              // Once a match has been found, don't continue to check the rest of the list
+              break;
             }
           }
         }
+      }
     }
 
     private void addHeaders(IHttpRequestResponse messageInfo, IRequestInfo rqInfo) {
-      List<String> headers = new ArrayList<String>(rqInfo.getHeaders());                                // TO-DO: add some logic to detect if a header in headersToAdd already exists in the request. If it does, skip and add next header
-      for (int j=0; j<headersToAdd.length; j++) {
-        headers.add(headersToAdd[j]);
+      // Get list of headers in request
+      List<String> headers = new ArrayList<String>(rqInfo.getHeaders());
+      // If checkForDuplicates is set to true, then check that a header has not been duplicated by the headersToAdd. If it has, remove original header
+      if (checkForDuplicates) {
+        for (String newHeader : headersToAdd) {
+          // Split key:value pair up to just the key
+          String[] key = newHeader.split(":");
+          // Make an empty list to record which headers will need to be removed
+          List<String> headersToRemove = new ArrayList<String>();
+          for (String oldHeader : headers) {
+            // If a header matches with the key, add it to the headersToRemove list
+            if (oldHeader.startsWith(key[0])) {
+              headersToRemove.add(oldHeader);
+            }
+          }
+          // Remove all duplicated headers
+          headers.removeAll(headersToRemove);
         }
+      }
+      // Iterate through the headersToAdd list, and add all new headers to the request
+      for (String newHeader : headersToAdd) {
+        headers.add(newHeader);
+      }
+      // Build new request
       String request = new String(messageInfo.getRequest());
       String messageBody = request.substring(rqInfo.getBodyOffset());
       byte[] updateMessage = helpers.buildHttpMessage(headers, messageBody.getBytes());
+      // Update existing request with the new request
       messageInfo.setRequest(updateMessage);
     }
-} 
+}
